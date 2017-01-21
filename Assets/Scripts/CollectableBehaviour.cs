ec90822dd6fb4cf0440140b11f2780024d5fb75e
Darko Kotoski <wbountyhunter@hotmail.com>
2017-01-21 19:42:34 +0100

    Added Consumables

---
+++ Assets/Scripts/CollectableBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableScript : MonoBehaviour {
+
+    public  GameObject Skin;
+    
+
+	// Use this for initialization
+	void Start () {
+        //Skin.GetComponent<Animator>().
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/CollectableSkinController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableSkinController : MonoBehaviour
+{
+
+    public CollectableColor animationColor;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (animationColor == CollectableColor.Blue)
+        {
+            GetComponent<Animator>().SetTrigger("BlueAnimation");
+        }
+
+        else if (animationColor == CollectableColor.Yellow)
+        {
+            GetComponent<Animator>().SetTrigger("YellowAnimation");
+        }
+
+        else
+        {
+            GetComponent<Animator>().SetTrigger("RedAnimation");
+        }
+
+    }
+
+}
+
+public enum CollectableColor
+{
+    Red = 1,
+    Blue = 2,
+    Yellow = 3
+}
